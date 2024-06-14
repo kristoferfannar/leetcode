968794f20eea3409e4a14081aa9244502efff864
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-06-14 23:17:52 +0000

    9: slightly improved golang solution

--- problems/0009. Palindrome Number/golang/solution.go
+++ problems/0009. Palindrome Number/golang/solution.go
@@ -11,18 +11,21 @@ func main() {
 	fmt.Printf("isPalindrome(12): %v\n", isPalindrome(12))
 }
 
+// Solutions:
+// 1. parse x into a string, reverse that string and compare the two
+// 2. parse x into a string, compare idxs i and len(string) - i - 1 until halfway there
 func isPalindrome(x int) bool {
 	xStr := strconv.Itoa(x)
-	var xRev string
-
-	for i := len(xStr) - 1; i >= 0; i-- {
-		xRev += string(xStr[i])
-	}
 
 	for i := 0; i < len(xStr); i++ {
-		if xStr[i] != xRev[i] {
+		if i*2 > len(xStr) {
+			return true
+		}
+
+		if xStr[i] != xStr[len(xStr)-1-i] {
 			return false
 		}
 	}
+
 	return true
 }
