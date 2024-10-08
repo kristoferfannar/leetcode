ea48dab73e5529181995d25cc7d29459bc47d753
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-10-08 01:57:51 -0400

    746: top-down
    I know top-down performs worse due to recursion,
    but it just looks more advanced

---
+++ problems/0746. Min Cost Climbing Stairs/python3/solution.py
@@ -0,0 +1,60 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.cost_dict = dict()
+
+    def minCost(self, cost: List[int]) -> int:
+        if not cost:
+            return 0
+        if len(cost) <= 2:
+            return cost[-1]
+
+        if (len(cost) - 2) not in self.cost_dict:
+            self.cost_dict[len(cost) - 2] = self.minCost(cost[:-1])
+        if (len(cost) - 3) not in self.cost_dict:
+            self.cost_dict[len(cost) - 3] = self.minCost(cost[:-2])
+
+        short = self.cost_dict[len(cost) - 2]
+        long = self.cost_dict[len(cost) - 3]
+
+        this = cost[-1] + min(long, short)
+        self.cost_dict[len(cost) - 1] = this
+
+        return this
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        lowest = min(self.minCost(cost), self.minCost(cost[:-1]))
+
+        for k, v in sorted(self.cost_dict.items(), key=lambda x: x[0]):
+            print(k, v)
+
+        return lowest
+
+
+if __name__ == "__main__":
+    s = Solution().minCostClimbingStairs([1, 2])
+    print(s)
+    print()
+    assert s == 1
+
+    s = Solution().minCostClimbingStairs([10, 15, 20])
+    print(s)
+    print()
+    assert s == 15
+
+    s = Solution().minCostClimbingStairs([100, 1, 1])
+    print(s)
+    print()
+    assert s == 1
+
+    s = Solution().minCostClimbingStairs([100, 1, 1, 100])
+    print(s)
+    print()
+    assert s == 2
+
+    s = Solution().minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
+    print(s)
+    print()
+    assert s == 6
