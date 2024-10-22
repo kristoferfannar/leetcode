3a3ab3d3d19c97d367d659d304ac60115ecb95d3
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-10-22 00:12:56 -0400

    3110: use bits and assert

--- problems/3110. Score of a String/c++/main.cpp
+++ problems/3110. Score of a String/c++/main.cpp
@@ -1,8 +1,6 @@
-#include <cmath>
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
   int scoreOfString(string s) {
@@ -11,12 +9,8 @@ public:
     for (int i = 0; i < s.size() - 1; i++) {
       score += abs(s[i] - s[i + 1]);
     }
-    cout << "score: " << score << "\n";
     return score;
   }
 };
 
-int main() {
-  auto s = Solution();
-  s.scoreOfString("hello");
-}
+int main() { assert(Solution().scoreOfString("hello") == 13); }
