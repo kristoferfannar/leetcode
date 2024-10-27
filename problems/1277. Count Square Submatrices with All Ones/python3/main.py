eef6e04cbb1000d6944a1122fc26b2436338ceb6
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-10-26 21:44:26 -0400

    1277
    somehow passed with this approach,
    even thought my own `time python3 main.py` tests
    showed the original implementation performed better??

--- problems/1277. Count Square Submatrices with All Ones/python3/main.py
+++ problems/1277. Count Square Submatrices with All Ones/python3/main.py
@@ -1,34 +1,80 @@
 from typing import List
+from numpy import array
+
+from test import lis as LIS
+from time import time
 
 
 class Solution:
-    def is_ones(self, matrix, start_x, start_y, last_x, last_y):
-        for x in range(start_x, last_x + 1):
-            for y in range(start_y, last_y + 1):
-                if matrix[y][x] == 0:
-                    return False
+    def row_sums(self, matrix: List[List[int]]) -> List[List[int]]:
+        sums = []
 
-        return True
+        for row in matrix:
+            row_sum = [row[0]]
+
+            for i in range(1, len(row)):
+                row_sum.append(row[i] + row_sum[i - 1])
+            sums.append(row_sum)
+
+        return sums
+
+    def area_sums(self, rsums: List[List[int]]) -> List[List[int]]:
+        c_sums = [rsums[0]]
+
+        for r in range(1, len(rsums)):
+            sums = []
+            for i in range(len(rsums[r])):
+                sums.append(rsums[r][i] + c_sums[r - 1][i])
+
+            c_sums.append(sums)
+
+        return c_sums
+
+    def is_ones(self, sums, x, y, d) -> bool:
+
+        prev = left = up = 0
+        if y - 1 >= 0 and x - 1 >= 0:
+            prev = sums[y - 1][x - 1]
+        if y - 1 >= 0:
+            up = sums[y - 1][x + d]
+        if x - 1 >= 0:
+            left = sums[y + d][x - 1]
+
+        curr = sums[y + d][x + d]
+
+        return curr - left - up + prev == (d + 1) * (d + 1)
 
     def countSquares(self, matrix: List[List[int]]) -> int:
         dim = min(len(matrix), len(matrix[0]))
+
+        # print(array(matrix))
+        start = time()
+        sums = self.row_sums(matrix)
+        # print()
+        # print(array(sums))
+        sums = self.area_sums(sums)
+        # print(array(sums))
+        print(f"initialized sums in {time() - start} sec")
+
         squares = 0
 
         for d in range(dim):
-            print(f"d-{d}")
             for y in range(len(matrix) - d):
-                last_y = y + d
                 for x in range(len(matrix[y]) - d):
-                    last_x = x + d
 
-                    if self.is_ones(matrix, x, y, last_x, last_y):
+                    if self.is_ones(sums, x, y, d):
                         squares += 1
-                    print(f"({x}, {y}) - ({last_x}, {last_y})")
 
         return squares
 
 
 if __name__ == "__main__":
-    s = Solution().countSquares([[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]])
+    # s = Solution().countSquares([[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]])
+    # print(s)
+    # assert s == 15
+
+    print(f"LIS = x-{len(LIS[0])} y-{len(LIS)}")
+
+    s = Solution().countSquares(LIS)
     print(s)
-    assert s == 15
+    assert s == 22859
