5e4ebd69b558070566acebe5cd67d394e92ce451
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-10-26 20:51:59 -0400

    1277: TLE
    give me 20 min

---
+++ problems/1277. Count Square Submatrices with All Ones/python3/main.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def is_ones(self, matrix, start_x, start_y, last_x, last_y):
+        for x in range(start_x, last_x + 1):
+            for y in range(start_y, last_y + 1):
+                if matrix[y][x] == 0:
+                    return False
+
+        return True
+
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        dim = min(len(matrix), len(matrix[0]))
+        squares = 0
+
+        for d in range(dim):
+            print(f"d-{d}")
+            for y in range(len(matrix) - d):
+                last_y = y + d
+                for x in range(len(matrix[y]) - d):
+                    last_x = x + d
+
+                    if self.is_ones(matrix, x, y, last_x, last_y):
+                        squares += 1
+                    print(f"({x}, {y}) - ({last_x}, {last_y})")
+
+        return squares
+
+
+if __name__ == "__main__":
+    s = Solution().countSquares([[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]])
+    print(s)
+    assert s == 15
