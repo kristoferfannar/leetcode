ad8c5ead49da1f5c36cc62cb2351e8bcd512aab2
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-12-10 14:06:43 -0500

    1748

---
+++ problems/1748. Sum of Unique Elements/python3/main.py
@@ -0,0 +1,7 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        return sum([k for k, v in Counter(nums).items() if v == 1])
