4a85d4ff2b0ccb68561ea2ba578e9fdd06da535b
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-01-25 00:48:46 -0500

    1480 in c++

---
+++ problems/1480. Running Sum of 1d Array/cpp/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> runningSum(vector<int> &nums) {
+    int currSum = 0;
+
+    vector<int> final;
+
+    for (auto i : nums) {
+      currSum += i;
+      final.push_back(currSum);
+    }
+    return final;
+  }
+};
+
+int main() {
+  auto s = Solution();
+
+  vector<int> expected = {1, 3, 6};
+  vector<int> in = {1, 2, 3};
+  vector<int> got = s.runningSum(in);
+
+  for (auto i : expected)
+    cout << i << " ";
+  cout << endl;
+
+  for (auto i : got)
+    cout << i << " ";
+  cout << endl;
+  // cout << "got: " << acc << endl;
+  // cout << "exp: " << accumulate(expected.begin(), expected.end(), string(""))
+  //      << endl;
+
+  assert(got == expected);
+}
