068bd4ef44eb7cc2e940acd70995ed0a2ac2e67d
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-04-21 23:52:48 -0400

    nasty 3sum in cpp

---
+++ problems/0015. 3Sum/cpp/sol.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  private:
+    int bsearch(vector<int> &nums, int target) {
+        int l = 0, h = nums.size() - 1, m;
+
+        while (l <= h) {
+            m = (l + h) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    const int MAX = 1e5;
+
+  public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+
+        set<vector<int>> found;
+        vector<vector<int>> vfound;
+        vector<int> snums{nums[0]};
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1])
+                snums.push_back(nums[i]);
+        }
+
+        vector<int> counter(MAX * 2 + 1, 0);
+        for (auto i : nums)
+            counter[i + MAX]++;
+
+        if (counter[MAX] >= 3) {
+            vector<int> base{0, 0, 0};
+            vfound.push_back(base);
+        }
+
+        for (int i = 0; i < snums.size(); i++) {
+            for (int j = i + 1; j < snums.size(); j++) {
+                int target = bsearch(snums, -(snums[i] + snums[j]));
+
+                if (target != -1) {
+                    auto count = 1 + (snums[target] == snums[i]) +
+                                 (snums[target] == snums[j]);
+
+                    if (counter[snums[target] + MAX] < count) {
+                        continue;
+                    }
+
+                    vector<int> f{snums[i], snums[j], snums[target]};
+                    sort(f.begin(), f.end());
+
+                    if (found.find(f) == found.end()) {
+                        vfound.push_back(f);
+                    }
+                    found.insert(f);
+                }
+            }
+        }
+
+        return vfound;
+    }
+};
